702a0a240e5fbfefb622660769eec15463980ddc
Guilherme Romero <50001571+guilherme1985@users.noreply.github.com>
2025-08-31 01:55:55 +0000

    Funçoes de capturar os valores Açoes e Bitcoin

---
+++ app/GetAcoes.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+import pandas as pd
+
+from datetime import datetime
+
+def get_acoes() -> pd.DataFrame:
+    # Top 10 ações no Ibovespa (por peso no índice)
+    top_10 = [
+        "PETR4.SA",  # Petrobras
+        "VALE3.SA",  # Vale
+        "ITUB4.SA",  # Itaú Unibanco
+        "BBDC4.SA",  # Bradesco
+        "B3SA3.SA",  # B3 
+        "WEGE3.SA",  # Weg (Equipamentos elétricos)
+        "ABEV3.SA",  # Ambev
+        "MGLU3.SA",  # Magazine Luiza
+        "RENT3.SA",  # Localiza
+        "BBAS3.SA"   # Banco do Brasil
+    ]
+    dfs = []
+
+    for a in top_10:
+        acao = yf.Ticker(a)
+        info = acao.info
+        preco = info['regularMarketPrice']
+        nome = info['shortName']
+        setor = info.get('sector', 'N/A')
+        dt_rq = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+        df = pd.DataFrame({
+            'ticker': [acao],
+            'nome': [nome],
+            'setor': [setor],
+            'preco': [preco],
+            'data_requisicao': [dt_rq]
+        })
+        
+        dfs.append(df)
+    
+    resultado = pd.concat(dfs, ignore_index=True)
+    return resultado
+
+# acoes = get_acoes()
+
+# print(acoes)
---
+++ app/GetBitcoin.py
@@ -0,0 +1,39 @@
+import requests as rq
+import pandas as pd
+import os
+import dotenv
+
+from datetime import datetime
+
+#Carrega variáveis de ambiente
+dotenv.load_dotenv()   
+URL_BITCOIN = os.getenv("URL_BITCOIN")
+
+res = rq.get(URL_BITCOIN)
+bc_price = res.json()
+
+def get_bitcoin_price() -> pd.DataFrame:
+
+    # Requisição para a API e conversao para json
+    res = rq.get(URL_BITCOIN)
+    bc_price = res.json()
+
+    # Extração
+    preco = float (bc_price['data']['amount'])
+    cripto = 'BTC'
+    moeda = 'USD'
+    dt_rq = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    # Dataframe
+    df = pd.DataFrame({
+        'preco': [preco],
+        'cripto': [cripto],
+        'moeda': [moeda],
+        'data_requisicao': [dt_rq]
+    })
+
+    return df
+
+# vlr_bitcoin = get_bitcoin_price()
+
+# print(vlr_bitcoin)
